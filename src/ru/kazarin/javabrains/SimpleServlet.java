3ebecebda62146d3e1e69e0735aeac16726fc7cf
KazarinAY <new_bazzz@mail.ru>
2015-10-28 21:34:15 +0400

    Request, Session and Context Part 2

--- src/ru/kazarin/javabrains/SimpleServlet.java
+++ src/ru/kazarin/javabrains/SimpleServlet.java
@@ -3,6 +3,7 @@ package ru.kazarin.javabrains;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -25,12 +26,15 @@ public class SimpleServlet extends HttpServlet {
 		String userName = request.getParameter("name");
 		PrintWriter writer = response.getWriter();
 		HttpSession session = request.getSession();
+		ServletContext context = request.getServletContext();
 		if (userName != "" && userName != null) {
 			session.setAttribute("savedUserName", userName);
+			context.setAttribute("savedUserName", userName);
 		}
 		
 		writer.println("Request name = " + userName);
 		writer.println("Session name = " + (String) session.getAttribute("savedUserName"));
+		writer.println("Context name = " + (String) context.getAttribute("savedUserName"));
 	}
 
 }
